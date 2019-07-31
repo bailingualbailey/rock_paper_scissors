e7f77a23bc9d18d6935f54e71b7cdce57186d426
Bailey <bailrey91@gmail.com>
2019-07-30 21:40:56 -0500

    updated style sheet and cleaned up functions.

--- rock.js
+++ rock.js
@@ -1,10 +1,12 @@
-function computerPlay () {
+let button = document.getElementById ("btn");
+let compScore = 0
+let playerScore = 0
+let roundIncrease = 0
+
+function computerPlay() {
     let plays = ["rock", "paper", "scissors"];
     return plays[Math.floor(Math.random() * ((plays.length -1) - 0 + 1))];
-}  
-
-  computerPlay();
- 
+} 
 
 let playerTurn = prompt('Rock, paper, or scissors?');
 let computerTurn = computerPlay();
@@ -18,11 +20,15 @@ if (playerTurn === 'rock') {
             break;
         case "paper":
             alert('Paper beats rock, YOU LOSE!');
+            compScore += 1
             break;
         case "scissors":
             alert('Rock beats scissors, YOU WIN!');
+            playerScore += 1
             break;
     }
+    roundIncrease += 1;
+    alert("round: " + roundIncrease + " Score: " + playerScore);
 }
 
 if (playerTurn === 'paper') {
@@ -31,14 +37,18 @@ if (playerTurn === 'paper') {
     switch(computerTurn) {
         case "rock":
             alert('Paper beats rock, YOU WIN!');
+            playerScore += 1
             break;
         case "paper":
             alert('Great minds think alike, TRY AGAIN');
             break;
         case "scissors":
             alert('Scissors beats paper, YOU LOSE!');
+            compScore += 1
             break;
     }
+    roundIncrease += 1;
+    alert("round: " + roundIncrease + " Your Score: " + playerScore + " Computer Score: " + compScore);
 }
 
 if (playerTurn === 'scissors') {
@@ -47,15 +57,22 @@ if (playerTurn === 'scissors') {
     switch(computerTurn) {
         case "rock":
             alert('Rock beats scissors, YOU LOSE!');
+            compScore += 1
             break;
         case "paper":
             alert('Scissors beats paper, YOU WIN!');
+            playerScore += 1
             break;
         case "scissors":
             alert('Great minds think alike, TRY AGAIN');
             break;
     }
+    roundIncrease += 1;
+    alert("round: " + roundIncrease + " Score: " + playerScore);
 }
+
+
+
 
 
 
